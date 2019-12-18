3354ae46e8fb7a839a7f5bed496ed07bb2f84456
faith orozco <faith_orozco@yahoo.com>
2019-12-18 14:47:21 -0800

    added bootstrap

--- wordpress/wp-content/themes/bootstrap-practice/index.php
+++ wordpress/wp-content/themes/bootstrap-practice/index.php
@@ -1,2 +1,6 @@
-<h1>This is an h1</h1>
-<h2>This is an h2</h2>
+<?php get_header(); ?>
+  <!-- Other Content here -->
+  <!-- <h1>This is your theme to practice BS4.</h1>
+  <h2>faith</h2> -->
+<?php get_footer(); ?>
+
